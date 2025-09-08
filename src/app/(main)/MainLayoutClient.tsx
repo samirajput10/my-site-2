d080d8d9e96c959c95e92062c938ed26047cd30d
Sami-ullah <brandboy553340@gmail.com>
2025-09-08 18:26:58 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk app

---
+++ src/app/(main)/MainLayoutClient.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
+import { PromotionalBanner } from '@/components/layout/PromotionalBanner';
+
+export function MainLayoutClient({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <PromotionalBanner />
+      <Header />
+      <main className="flex-grow bg-background">{children}</main>
+      <Footer />
+    </div>
+  );
+}
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,18 +1,9 @@
-import { Header } from '@/components/layout/Header';
-import { Footer } from '@/components/layout/Footer';
-import { PromotionalBanner } from '@/components/layout/PromotionalBanner';
+import { MainLayoutClient } from './MainLayoutClient';
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen flex-col">
-      <PromotionalBanner />
-      <Header />
-      <main className="flex-grow bg-background">{children}</main>
-      <Footer />
-    </div>
-  );
+  return <MainLayoutClient>{children}</MainLayoutClient>;
 }
