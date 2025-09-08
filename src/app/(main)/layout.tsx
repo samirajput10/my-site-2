1e1d65dbbb90b444cef1f10b789b5fdc0f2f6271
Sami-ullah <brandboy553340@gmail.com>
2025-09-08 16:42:02 +0000

    remove it (_for element <Button>_)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
-import { ChatButton } from '@/components/layout/ChatButton';
 import { PromotionalBanner } from '@/components/layout/PromotionalBanner';
 
 export default function MainLayout({
@@ -14,7 +13,6 @@ export default function MainLayout({
       <Header />
       <main className="flex-grow bg-background">{children}</main>
       <Footer />
-      <ChatButton />
     </div>
   );
 }
