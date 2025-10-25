de373c62c82c11527aa18d78aaf13a5c8a87628f
Sami-ullah <brandboy553340@gmail.com>
2025-10-25 18:02:16 +0000

    Try fixing this error: `Console Error: React does not recognize the `off

--- src/components/icons/PackageOpen.tsx
+++ src/components/icons/PackageOpen.tsx
@@ -18,10 +18,10 @@ const pathVariants: Variants = {
 
 const boxVariants: Variants = {
   initial: {
-    offsetDistance: "0%",
+    pathLength: 0,
   },
   hover: {
-    offsetDistance: "100%",
+    pathLength: 1,
     transition: {
       duration: 1,
       ease: "easeInOut",
@@ -76,10 +76,6 @@ const PackageOpen = ({
           cy="12"
           r="1"
           fill="currentColor"
-          style={{
-            offsetPath:
-              "path('M12 22v-9 M15.17 2.21a1.67 1.67 0 0 1 1.63 0L21 4.57a1.93 1.93 0 0 1 0 3.36L8.82 14.79a1.655 1.655 0 0 1-1.64 0L3 12.43a1.93 1.93 0 0 1 0-3.36z M20 13v3.87a2.06 2.06 0 0 1-1.11 1.83l-6 3.08a1.93 1.93 0 0 1-1.78 0l-6-3.08A2.06 2.06 0 0 1 4 16.87V13 M21 12.43a1.93 1.93 0 0 0 0-3.36L8.83 2.2a1.64 1.64 0 0 0-1.63 0L3 4.57a1.93 1.93 0 0 0 0 3.36l12.18 6.86a1.636 1.636 0 0 0 1.63 0z')",
-          }}
           variants={boxVariants}
         />
       </motion.svg>
